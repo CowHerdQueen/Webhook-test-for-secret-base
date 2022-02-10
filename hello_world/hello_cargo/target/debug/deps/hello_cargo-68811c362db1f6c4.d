0a6dbe5cdb34408fb62c20b780ab5314ed3dc515
Trans Cockroach <78016627+CowHerdQueen@users.noreply.github.com>
2022-02-09 23:18:31 -0800

    Add files via upload

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-68811c362db1f6c4.d
@@ -0,0 +1,5 @@
+E:\Rust_Programming\projects\hello_world\hello_cargo\target\debug\deps\hello_cargo-68811c362db1f6c4.rmeta: src\main.rs
+
+E:\Rust_Programming\projects\hello_world\hello_cargo\target\debug\deps\hello_cargo-68811c362db1f6c4.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\Rust_Programming\projects\hello_world\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\Rust_Programming\projects\hello_world\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
